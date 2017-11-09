84326dbc4d68ad99566179cb8c56feb2d6b76daf
Célestin Matte <celestin.matte@gmail.com>
2017-11-09 15:19:12 +0100

    Fix panoptiphone_file.sh

--- panoptiphone_file.sh
+++ panoptiphone_file.sh
@@ -2,4 +2,8 @@
 
 file="$1"
 
-tshark -V -r "$file" -l -Y "wlan.fc.type_subtype == 0x4 && not _ws.malformed" -T fields -e frame.time -e wlan.sa -e wlan.seq -e wlan_mgt.ssid -e wps.uuid_e -e wlan_mgt.tag.number -e wlan_mgt.supported_rates -e wlan_mgt.extended_supported_rates -e wlan_mgt.ht.capabilities -e wlan_mgt.ht.ampduparam -e wlan_mgt.ht.mcsset.rxbitmask -e wlan_mgt.htex.capabilities -e wlan_mgt.txbf -e wlan_mgt.asel -e wlan_mgt.extcap -e wlan_mgt.interworking.access_network_type -e radiotap.dbm_antsignal -E separator=";" -E quote=d | python -u panoptiphone.py
+for i in "$@"
+do
+    echo "$i"
+    tshark -r "$i" -T pdml | python panoptiphone.py -xg
+done
